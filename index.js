8d58180d49823e12adc0ab7484000056c4fe8544
jamesmutune <tunekivasu01@gmail.com>
2024-01-15 14:34:04 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// code your solution here
+function superbowlWin(record) {
+    const winningYear = record.find(item => item.result === "W");
+    if (winningYear) {
+      return winningYear.year;
+    } else {
+      return undefined;
+    }
+  }
